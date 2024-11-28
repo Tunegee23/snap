5781b8ae8f855271ab0367ed9e508a7dac95b3c6
Tunegee23 <ogunsanya.opeyemi23@gmail.com>
2024-11-28 08:11:19 +0100

    snap

--- snap/src/Pages/Homepage.jsx
+++ snap/src/Pages/Homepage.jsx
@@ -8,7 +8,7 @@ function Homepage() {
 
 
   return (
-    <div className='xl:h-[760px] md:h-[650px] bg-[#4cb2da79] '>
+    <div className='  bg-[#4cb2da79] '>
 
       <div className='mx-auto px-4 py-6 justify-between  flex items-center '>
 
@@ -37,15 +37,18 @@ function Homepage() {
 
       </div>
 
-      <div className=' md:flex  lg:ml-[5rem] sm:ml-[6rem] lg:mr-[5rem] xl:mt-[5rem] lg:gap-[6rem] justify-evenly md:ml-[4rem] md:gap-[5rem] mt-[2rem] md:mt-[3rem]  px-4 py-6 '>
+      <div className=' xl:flex  xl:mt-[5rem] lg:gap-[6rem] justify-evenly md:gap-[5rem] mt-[2rem] md:mt-[3rem]  px-4 py-6 '>
         <div className='lg:mt-[2rem] md:mt-[3rem]'>
-          <h1 className='font-[900] w-[330px] text-[#000000d1]  leading-[3rem] text-[40px]'>Make remote work</h1>
-          <p className='md:w-[350px] sm:w-[450px] text-[#000000d1] w-[350px] lg:w-[450px] font-[400] text-[20px] mt-[1rem]'>Get your team in sync, no matter your location stramline processes, create team rituals, and watch productivity.</p>
-          <button className='mt-[1rem] text-[white] border-[1px] border-[solid] rounded-[10px] bg-[black] w-[100px] h-[40px]'>Learn more</button>
+          <h1 className='font-[900]  w-full lg:text-[50px] text-center text-[#000000d1]  leading-[3rem] text-[40px]'>Make remote work</h1>
+          <p className=' text-[#000000d1] text-center w-full lg:text-[30px] font-[400] sm:text-[25px] text-[20px] mt-[1rem]'>Get your team in sync, no matter your location stramline processes, create team rituals, and watch productivity.</p>
+          <div className='flex items-center justify-center'>
+            <button className='mt-[1rem] text-[white]  border-[1px] border-[solid] rounded-[10px] bg-[black] w-[100px] h-[40px]'>Learn more</button>
+          </div>
+          
           
         </div>
-        <div className=''>
-          <img src={image} className=' w-[300px]  mt-[3rem] md:mt-[4px] lg:w-[350px] ' alt=''></img>
+        <div className=' flex items-center mt-[2rem] justify-center'>
+          <img src={image} className='w-[350px] sm:w-[600px] ' alt=''></img>
         </div>
 
       </div>
